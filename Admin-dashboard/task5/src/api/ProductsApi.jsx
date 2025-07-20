6514c466e9f00291d265f74cb092faa3d8ef837f
HebaElgohary <hebasamir535@gmail.com>
2025-07-21 02:33:46 +0300

    added more files

--- Admin-dashboard/task5/src/api/ProductsApi.jsx
+++ Admin-dashboard/task5/src/api/ProductsApi.jsx
@@ -1,16 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrl='http://localhost:3002/products'
+const baseUrl = "/.netlify/functions/api";
 
+const getAllProducts = () => axios.get(baseUrl);
+const getProductById = (productId) => axios.get(`${baseUrl}/${productId}`);
+const addProduct = (product) => axios.post(baseUrl, product);
+const deleteProduct = (productId) => axios.delete(`${baseUrl}/${productId}`);
+const updateProduct = (productId, product) =>
+  axios.put(`${baseUrl}/${productId}`, product);
 
-const  getAllProducts=()=> axios.get(baseUrl)
-const  getProductById=(productId)=>axios.get(`${baseUrl}/${productId}`)
-const addProduct=(product)=>axios.post(baseUrl,product)
-const deleteProduct=(productId)=>axios.delete(`${baseUrl}/${productId}`)
-const updateProduct=(productId,product)=>axios.put(`${baseUrl}/${productId}`,product)
-
-
-
-
-
-export{getAllProducts,getProductById,addProduct,updateProduct,deleteProduct}
+export {
+  getAllProducts,
+  getProductById,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+};
